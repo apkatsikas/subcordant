531bc88b1b55b28206e7588f9c7bf5b01f1ff5ae
apkatsikas <apkatsikas@gmail.com>
2025-05-12 22:07:44 -0400

    Add test

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -1,17 +1,19 @@
 package runner_test
 
 import (
+	"github.com/apkatsikas/subcordant/runner"
 	. "github.com/onsi/ginkgo/v2"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("runner", func() {
-
+	var subcordantRunner *runner.SubcordantRunner
 	BeforeEach(func() {
-
+		subcordantRunner = &runner.SubcordantRunner{}
 	})
 
 	It("will run", func() {
-
+		subcordantRunner.HandlePlay("foobar")
+		Expect(1).To(Equal(1))
 	})
 })
