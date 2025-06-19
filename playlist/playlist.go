d90953bcb0666270f1d5d695e678a5456e3a9559
apkatsikas <apkatsikas@gmail.com>
2025-06-19 19:59:02 -0400

    Fix race conditions

--- playlist/playlist.go
+++ playlist/playlist.go
@@ -2,28 +2,38 @@ package playlist
 
 import (
 	"slices"
+	"sync"
 
 	"github.com/apkatsikas/subcordant/types"
 )
 
 type PlaylistService struct {
+	mu       sync.Mutex
 	playlist []types.Track
 }
 
 func (ps *PlaylistService) Add(track types.Track) {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
 	ps.playlist = append(ps.playlist, track)
 }
 
 func (ps *PlaylistService) GetPlaylist() []types.Track {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
 	return ps.playlist
 }
 
 func (ps *PlaylistService) FinishTrack() {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
 	if len(ps.playlist) > 0 {
 		ps.playlist = slices.Delete(ps.playlist, 0, 1)
 	}
 }
 
 func (ps *PlaylistService) Clear() {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
 	ps.playlist = []types.Track{}
 }
